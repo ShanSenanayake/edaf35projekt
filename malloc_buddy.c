cf4b608ab4b508c73a08e29380fd4cb019cee69e
ShanSenanayake <senanayake92@gmail.com>
2015-05-13 16:12:38 +0200

    malloc stuff

--- malloc_buddy.c
+++ malloc_buddy.c
@@ -23,18 +23,64 @@ block_t* free_list[K_VALUE];
 
 
 
+block_t* split_block(block_t* block)
+{
+	char kval = block->kval;
+	block_t* next = block->next;
+	if(next){
+		next->prev = NULL;
+		freelist[kval] = next;
+	}
+	block->reserved = 1;
+	block->next = NULL;
+	block->next = NULL;			
+}
+
+/*adjust the size to be a multiple by 2. The number of shifts are returned in index*/
+size_t adjust_size(size_t size, int* index)
+{
+	size_t new_size = 1;
+	*index = 0;
+	while(new_size<=size+META_SIZE){
+		new_size<<=1;
+		++(*index);
+	}
+
+	return new_size;	
+}
+
 
 void* malloc(size_t size)
 {
 	block_t* block;
-	size
 
 	if(size<=0)
 		return NULL;
 
+	int index;
+	size = adjust_size(size,&index);
+
+
 	if(global_memory){
+		block_t* next;
+		if(freelist[index]){
+			block = freelist[index];
+
+			next = block->next;
+			if(next){
+				next->prev = NULL;
+				freelist[index] = next;			
+			}else{
+				freelist[index] = NULL;			
+			}
+			
+			block->reserved = 1;
+			block->next = NULL;
+			block->next = NULL;	
+			return (block+1);
+		}
 	
-	}else{
+	}else{ /*first time*/
 		global_memory = sbrk(1<<K_VALUE);
 		block = global_memory;
 
@@ -48,6 +94,8 @@ void* malloc(size_t size)
 
 		free_list[K_VALUE-1] = block;	
 	}	
+
+		
 
 
 
