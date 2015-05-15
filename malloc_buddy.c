f3bab4dccedfc190ef9b1580de3c1ac29383bd3d
ShanSenanayake <senanayake92@gmail.com>
2015-05-15 17:47:25 +0200

    buddy done?

--- malloc_buddy.c
+++ malloc_buddy.c
@@ -18,24 +18,10 @@ struct block_t{
 #define K_VALUE		(22)
 
 void* global_memory = NULL;
-block_t* free_list[K_VALUE];
+block_t* free_list[K_VALUE+1];
 
 
 
-
-block_t* split_block(block_t* block)
-{
-	char kval = block->kval;
-	block_t* next = block->next;
-	
-	if(next){
-		next->prev = NULL;
-		freelist[kval] = next;
-	}
-	block->reserved = 1;
-	block->next = NULL;
-	block->next = NULL;			
-}
 
 /*adjust the size to be a multiple by 2. The number of shifts are returned in index*/
 size_t adjust_size(size_t size, int* index)
@@ -50,6 +36,42 @@ size_t adjust_size(size_t size, int* index)
 	return new_size;	
 }
 
+block_t* remove_first(int index)
+{
+
+	block_t* block 		=	freelist[index];
+	block_t* next		=	block->next;
+	if(next){
+		next->prev 		= 	NULL;
+		freelist[index] = 	next;
+	}else{
+
+		freelist[index] = 	NULL;			
+	}
+	
+	block->reserved 	= 	1;
+	block->next 		= 	NULL;
+	block->next 		= 	NULL;	
+
+	return block;
+}
+
+
+block_t* split_block(block_t* block)
+{
+	char kval = block->kval-1;
+
+	block_t* new_block = block + (1 << kval);
+	new_block->kval = kval;
+	new_block->reserved = 0;
+
+	block->kval = kval;
+
+	return new_block;
+		
+}
+
+
 
 void* malloc(size_t size)
 {
@@ -63,24 +85,10 @@ void* malloc(size_t size)
 
 
 	if(global_memory){
-		block_t* next;
 		if(freelist[index]){
-			block = freelist[index];
-
-			next = block->next;
-			if(next){
-				next->prev = NULL;
-				freelist[index] = next;			
-			}else{
-				freelist[index] = NULL;			
-			}
-			
-			block->reserved = 1;
-			block->next = NULL;
-			block->next = NULL;	
-			return (block+1);
+			return (remove_first(block)+1);
 		}
-	
+
 	}else{ /*first time*/
 		global_memory = sbrk(1<<K_VALUE);
 		block = global_memory;
@@ -96,16 +104,122 @@ void* malloc(size_t size)
 		free_list[K_VALUE-1] = block;	
 	}	
 
-		
+	int new_index = index;
+	while(!freelist[new_index++])
+		;
+	while(new_index > index){
+		block = freelist[new_index];
+		block_t* next = block->next;
+		if(next){
+			next->prev = NULL;
+			freelist[new_index] = next;
+		}
+
+		block->next = NULL;
+		block_t new_block* = split_block(block);
+		--new_index;
+		block_t old_first = freelist[new_index];
+		if(old_first){
+			old_first->prev = block;
+			block->next = old_first;
+		}
+		new_block->next = block;
+		block->prev = new_block;
+		freelist[new_index] = new_block;
+		block = new_block;
+	}
 
 
-
-	return (block+1);
+	return (remove_first(block)+1);
 }
 
+
+
+/*buddy = start + ((pointer - start) ^ (1 << K));*/
+block_t* find_buddy(block_t* block)
+{
+	if(block->kval = K_VALUE-1)
+		return NULL
+
+	block_t* buddy = global_memory + ((block-global_memory) ^ (1<<(block->kval)));
+	if(!buddy->free || buddy->kval != block->kval)
+		return NULL;
+
+	return buddy;
+}
+
+void remove_from_freelist(block_t* block)
+{
+	block->next->prev = block->prev;
+	block->prev->next = block->next;
+	block->prev = NULL;
+	block->next = NULL;
+}
+
+block_t* deallocate(block_t* block)
+{
+	block_t* buddy = find_buddy(block);
+	if(buddy){
+		remove_from_freelist(block);
+		if(block>buddy){
+			++(buddy->kval);
+			block = buddy;
+		}else{
+			++(block->kval);
+		}
+		deallocate(block);
+	}else{
+		return block;
+	}
+}
 
 
 void free(void* ptr)
 {
+	block_t* block = (block_t*)(ptr)-1;
+	block->reserved = 0;
+	deallocate(block);
+	char kval = block->kval;
+	block->next = freelist[kval];
+	block->next->prev = block;
+	freelist[kval] = block;
+
 }
+
+void* calloc(size_t count, size_t size)
+{
+	size_t tot_size = count*size;
+	void* ptr = malloc(tot_size);
+
+	if(!ptr)
+		return NULL;
+
+	memset(ptr,0,tot_size);
+	return ptr;
+}
+
+void* realloc(void* ptr, size_t size)
+{
+	if(!ptr)
+		return malloc(size);
+
+	if(size <=0)
+		free(ptr);
+	
+	block_t* block = (block_t*)ptr-1;
+	size = adjust_size(size);
+
+	if(block->size>=size)
+		return ptr;
+
+	void* ptr2 = malloc(size);
+	if(!ptr2)
+		return NULL;
+
+	memcpy(ptr2 ,ptr,block->size-META_SIZE);
+	free(ptr);
+	return ptr2;
+}
+
+
 
